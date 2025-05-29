c5bab2d93e059f81b9301c785ac3c3d8eb9954e4
ghosh <ghosharghya918@gmail.com>
2025-05-29 20:49:46 +0530

    Add project files.

---
+++ collegeApp/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace collegeApp.Controllers
+{
+    [Route("api/studentController")]
+    [ApiController]
+    public class StudentController : Controller
+    {
+        [HttpGet]
+       public string GetStudent()
+        {
+            return "student name 1";
+        }
+    }
+}
