f4f9beb1ac9b23bb97b9917676f74398c84017c0
ghosh <ghosharghya918@gmail.com>
2025-06-13 21:34:26 +0530

    Creating Entity Classes EF Code First

---
+++ collegeApp/Data/CollegeDBContext.cs
@@ -0,0 +1,10 @@
+﻿using collegeApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace collegeApp.Data
+{
+    public class CollegeDBContext : DbContext
+    {
+        DbSet<Student> students { get; set; }
+    }
+}
--- collegeApp/Model/Student.cs
+++ collegeApp/Model/Student.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace collegeApp.Model
 {
     public class Student
     {
-        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string StudentName { get; set; }
         public string Email { get; set;}
