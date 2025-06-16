8dab6ebc6dcb1b3f2d0c71642f2658b87fe14647
ghosh <ghosharghya918@gmail.com>
2025-06-16 20:18:12 +0530

    Migrate tables with proper schema Datatype

--- collegeApp/Data/CollegeDBContext.cs
+++ collegeApp/Data/CollegeDBContext.cs
@@ -27,7 +27,17 @@ namespace collegeApp.Data
                     Email = "bappa@gmail.com",
                     DOB = new DateTime(2020, 1,12)
                 },
-            }); ;
+            });
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(n => n.StudentName).IsRequired();
+                entity.Property(n => n.StudentName).HasMaxLength(50);
+                entity.Property(n => n.Address).IsRequired(false);
+                entity.Property(n => n.Address).HasMaxLength(50);
+                entity.Property(n => n.Email).IsRequired();
+                entity.Property(n => n.Email).HasMaxLength(60);
+            });
         }
     }
 }
---
+++ collegeApp/Migrations/20250614112523_MigrateWithProperSchemaDatatype.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace collegeApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateWithProperSchemaDatatype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentName",
+                table: "students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "students",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "students",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StudentName",
+                table: "students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
